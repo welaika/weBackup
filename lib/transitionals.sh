5d50e1d3d7a8000eb6aadbbcfe296390b9c80c17
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2013-01-10 15:42:00 +0100

    try fixing: syntax error near unexpected token `}'

--- lib/transitionals.sh
+++ lib/transitionals.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 # This is a library for functions to handle updates between versions
 # of the script when we can't implement backwards compatible features.
 # they all must be called within the transitionals() function that is
 # placed at the beginning of the backup.sh script.
 
-function transitionals(){
+function transitionals() {
   tr__perhostretention
   return 0
 }
@@ -11,26 +13,24 @@ function transitionals(){
 # tr__perhostretention
 # no args
 # check if the host.conf files supports the per host retention or log down an alert
-function tr__perhostretention(){
+function tr__perhostretention() {
   for host in $HOSTS; do
     . ${CONF_DIR}/${host}/host.conf
-    ${servconf[5]} || (
-        log <<EOT
-          [ALERT]
-          ${host}/host.conf seems out of date, since it has not retention
-          setting. Please take a look and if it has not add these lines at
-          the end of the array inside the file:
+    ${servconf[5]} || log <<EOT
+"[ALERT]
+${host}/host.conf seems out of date, since it has not retention
+setting. Please take a look and if it has not add these lines at
+the end of the array inside the file:
 
-          #retention
-          ##########
-          # Set the retention of the backups for the specific host in weeks. MUST be an
-          # integer (e.g.: 2 stands for 2 weeks). Incremental backups older than $retention
-          # will be deleted. Default is 2 week.
-          [5]=2
+#retention
+##########
+# Set the retention of the backups for the specific host in weeks. MUST be an
+# integer (e.g.: 2 stands for 2 weeks). Incremental backups older than $retention
+# will be deleted. Default is 2 week.
+[5]=2
 
-          You can follow template.tpl/host.conf as example. If you won't do this,
-          the retention time will be set as 2 weeks.
-
+You can follow template.tpl/host.conf as example. If you won't do this,
+the retention time will be set as 2 weeks.
 EOT
-      )
+
 }
