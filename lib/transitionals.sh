05b82459cc028e89bd94183e12ddab24a9720c58
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2013-01-10 16:29:34 +0100

    and i hate also read manpage!

--- lib/transitionals.sh
+++ lib/transitionals.sh
@@ -15,24 +15,24 @@ function tr__perhostretention(){
   for host in $HOSTS; do
     . ${CONF_DIR}/${host}/host.conf
     [[ ${servconf[5]} ]] || (
-        message=<<EOT
-          [ALERT]
-          ${host}/host.conf seems out of date, since it has not retention
-          setting. Please take a look and if it has not add these lines at
-          the end of the array inside the file:
+      read -r -d '' message <<EOT
+[ALERT]
+${host}/host.conf seems out of date, since it has not retention
+setting. Please take a look and if it has not add these lines at
+the end of the array inside the file:
 
-          #retention
-          ##########
-          # Set the retention of the backups for the specific host in weeks. MUST be an
-          # integer (e.g.: 2 stands for 2 weeks). Incremental backups older than $retention
-          # will be deleted. Default is 2 week.
-          [5]=2
+#retention
+##########
+# Set the retention of the backups for the specific host in weeks. MUST be an
+# integer (e.g.: 2 stands for 2 weeks). Incremental backups older than $retention
+# will be deleted. Default is 2 week.
+[5]=2
 
-          You can follow template.tpl/host.conf as example. If you won't do this,
-          the retention time will be set as 2 weeks.
+You can follow template.tpl/host.conf as example. If you won't do this,
+the retention time will be set as 2 weeks.
 
 EOT
-        log $message 1 1
+        log "$message" 1 1
       )
   done
 }
