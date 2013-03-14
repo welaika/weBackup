086e2aee63e44343e326f488ade507a3601b9d6b
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2013-03-14 15:40:56 +0100

    deleted unusefull files

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script for first deploy of the script. Scope of the script is to:
-#+check the presence of rdiff-backup on the system
-#+check the presence of ssh
-#+check the presence of the $backup_dir (source it from configure)
-#   if not create it
-#   if unset/null warn the user
-
-
