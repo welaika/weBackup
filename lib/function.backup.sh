288445b5ff81f0b4a304521064b23def58c949fb
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2016-08-22 13:03:52 +0200

    fix existence check

--- lib/function.backup.sh
+++ lib/function.backup.sh
@@ -38,7 +38,7 @@ function backup() {
 
     perhost_conf_parser
 
-    if ${servconf[6]}; then
+    if [[ ${servconf[6]} ]]; then
       local CURRENT_BACKUP_DIR=${BACKUP_DIR}/${servconf[6]}
     else
       local CURRENT_BACKUP_DIR=${BACKUP_DIR}
@@ -97,7 +97,7 @@ function backup() {
 }
 
 function dry_run() {
-  if ${servconf[6]}; then
+  if [[ ${servconf[6]} ]]; then
     local CURRENT_BACKUP_DIR=${BACKUP_DIR}/${servconf[6]}
   else
     local CURRENT_BACKUP_DIR=${BACKUP_DIR}
@@ -127,7 +127,7 @@ function dry_run() {
 # delete_older ( $host )
 # Execs rdiff-backup --remove-older-than ${__ret} for the specified $host
 function delete_older() {
-  if ${servconf[6]}; then
+  if [[ ${servconf[6]} ]]; then
     local CURRENT_BACKUP_DIR=${BACKUP_DIR}/${servconf[6]}
   else
     local CURRENT_BACKUP_DIR=${BACKUP_DIR}
@@ -169,7 +169,7 @@ function test_conf_dir(){
 # Test if backup destination directory exists and if not
 #+create it.
 function test_dest_dir() {
-  if ${servconf[6]}; then
+  if [[ ${servconf[6]} ]]; then
     local dir=${BACKUP_DIR}/${servconf[6]}/${1}
   else
     local dir=${BACKUP_DIR}/${1}
